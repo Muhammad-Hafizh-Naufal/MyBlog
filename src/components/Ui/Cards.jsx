9738dec99ff6e583a1029d2f0ba4d162c357acad
@Hafizh_Naufal <noval1122567@gmail.com>
2024-09-26 23:53:57 +0700

    fix err blog

--- src/components/Ui/Cards.jsx
+++ src/components/Ui/Cards.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Cards() {
   return (
     <>
@@ -41,156 +43,12 @@ export default function Cards() {
             </div>
 
             <div className="sm:flex sm:items-end sm:justify-end">
-              <a
-                href="/singlepage"
+              <Link
+                to={"/singlepage"}
                 className="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400"
               >
                 Read Blog
-              </a>
-            </div>
-          </div>
-        </article>
-        <article className="flex md:max-w-3xl  bg-white transition hover:shadow-xl">
-          <div className="rotate-180 p-2 [writing-mode:_vertical-lr]">
-            <time
-              datetime="2022-10-10"
-              className="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900"
-            >
-              <span>2024</span>
-              <span className="w-px flex-1 bg-gray-900/10"></span>
-              <span>Sep 26</span>
-            </time>
-          </div>
-
-          <div className="hidden sm:block sm:basis-56">
-            <img
-              alt=""
-              src="/bg/black-white-background-2.jpg"
-              className="aspect-square h-full w-full object-cover"
-            />
-          </div>
-
-          <div className="flex flex-1 flex-col justify-between">
-            <div className="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
-              <a href="#">
-                <h3 className="font-bold uppercase text-gray-900">
-                  Finding the right guitar for your style - 5 tips
-                </h3>
-              </a>
-
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Recusandae dolores, possimus pariatur animi temporibus nesciunt
-                praesentium dolore sed nulla ipsum eveniet corporis quidem,
-                mollitia itaque minus soluta, voluptates neque explicabo tempora
-                nisi culpa eius atque dignissimos. Molestias explicabo corporis
-                voluptatem?
-              </p>
-            </div>
-
-            <div className="sm:flex sm:items-end sm:justify-end">
-              <a
-                href="#"
-                className="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400"
-              >
-                Read Blog
-              </a>
-            </div>
-          </div>
-        </article>
-        <article className="flex md:max-w-3xl  bg-white transition hover:shadow-xl">
-          <div className="rotate-180 p-2 [writing-mode:_vertical-lr]">
-            <time
-              datetime="2022-10-10"
-              className="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900"
-            >
-              <span>2024</span>
-              <span className="w-px flex-1 bg-gray-900/10"></span>
-              <span>Sep 26</span>
-            </time>
-          </div>
-
-          <div className="hidden sm:block sm:basis-56">
-            <img
-              alt=""
-              src="/bg/black-white-background-2.jpg"
-              className="aspect-square h-full w-full object-cover"
-            />
-          </div>
-
-          <div className="flex flex-1 flex-col justify-between">
-            <div className="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
-              <a href="#">
-                <h3 className="font-bold uppercase text-gray-900">
-                  Finding the right guitar for your style - 5 tips
-                </h3>
-              </a>
-
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Recusandae dolores, possimus pariatur animi temporibus nesciunt
-                praesentium dolore sed nulla ipsum eveniet corporis quidem,
-                mollitia itaque minus soluta, voluptates neque explicabo tempora
-                nisi culpa eius atque dignissimos. Molestias explicabo corporis
-                voluptatem?
-              </p>
-            </div>
-
-            <div className="sm:flex sm:items-end sm:justify-end">
-              <a
-                href="#"
-                className="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400"
-              >
-                Read Blog
-              </a>
-            </div>
-          </div>
-        </article>
-        <article className="flex md:max-w-3xl  bg-white transition hover:shadow-xl">
-          <div className="rotate-180 p-2 [writing-mode:_vertical-lr]">
-            <time
-              datetime="2022-10-10"
-              className="flex items-center justify-between gap-4 text-xs font-bold uppercase text-gray-900"
-            >
-              <span>2024</span>
-              <span className="w-px flex-1 bg-gray-900/10"></span>
-              <span>Sep 26</span>
-            </time>
-          </div>
-
-          <div className="hidden sm:block sm:basis-56">
-            <img
-              alt=""
-              src="/bg/black-white-background-2.jpg"
-              className="aspect-square h-full w-full object-cover"
-            />
-          </div>
-
-          <div className="flex flex-1 flex-col justify-between">
-            <div className="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
-              <a href="#">
-                <h3 className="font-bold uppercase text-gray-900">
-                  Finding the right guitar for your style - 5 tips
-                </h3>
-              </a>
-
-              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Recusandae dolores, possimus pariatur animi temporibus nesciunt
-                praesentium dolore sed nulla ipsum eveniet corporis quidem,
-                mollitia itaque minus soluta, voluptates neque explicabo tempora
-                nisi culpa eius atque dignissimos. Molestias explicabo corporis
-                voluptatem?
-              </p>
-            </div>
-
-            <div className="sm:flex sm:items-end sm:justify-end">
-              <a
-                href="#"
-                className="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400"
-              >
-                Read Blog
-              </a>
+              </Link>
             </div>
           </div>
         </article>
